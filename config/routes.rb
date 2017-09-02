2dd036dd92d875c3a1b29509a60a620da8c55b60
Jeroen Masschelein <jeroen.masschelein@gmail,com>
2017-09-02 14:36:15 -0400

    Jobs Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'jobs/index'
+
+  get 'jobs/show'
+
+  get 'jobs/new'
+
+  get 'jobs/edit'
+
   get 'welcome/index'
 
   get 'welcome/about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ require 'random_data'
+
+ # Create Posts
+ 50.times do
+ # #1
+   Job.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     description:   RandomData.random_paragraph,
+     deadline: "17/9/2015",
+     budget: 20
+   )
+ end
+ jobs = Job.all
+ 
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     job: jobs.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+ 
+ puts "Seed finished"
+ puts "#{Job.count} posts created"
+ puts "#{Comment.count} comments created"
